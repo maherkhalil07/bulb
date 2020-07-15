a0bb10ee4ce18dce0848b3b1b59a51e4442191f5
Maher Khalil <maherkhalil@outlook.com>
2020-07-15 14:42:43 +0200

    adding comments

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ function operateLight(x) {
     if (x == on) {
         document.getElementById("imagelight").src = "/images/pic_bulbon.gif";
         document.getElementById("body").style = "background-color: royalblue;";
-    } if (x == off) {
+    }
+    // can ignore second if and use else only in place of off
+    if (x == off) {
         document.getElementById("imagelight").src = "/images/pic_bulboff.gif";
         document.getElementById("body").style = "background-color: balck;";
     } else {
